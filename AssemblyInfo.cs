9810540c6459142402910ef405cf8466f33f2b84
kashyapnishant779-cmd <kashyapnishant779@gmail.com>
2026-06-06 18:42:40 +0530

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CorelDRAW Design Finder")]
+[assembly: AssemblyDescription("Tool to scan folders, extract CDR preview, compute hashes and locate duplicates.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Creative Tools")]
+[assembly: AssemblyProduct("CorelDRAW Design Finder")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("49a39be9-1f18-4ee4-b6e9-c660b4ac6cf2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
